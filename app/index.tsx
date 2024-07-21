9b959630b13a5be134e34af3260f0b9709b738f5
John Picker <john@ossistemes.com>
2024-07-21 20:31:48 +0200

    Tailwind config

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,4 @@
 import * as React from "react";
-import { Button, Text, View } from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import MovieScreen from "@/screens/MovieScreen";
